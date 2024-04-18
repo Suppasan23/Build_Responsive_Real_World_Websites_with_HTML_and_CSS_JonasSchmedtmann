23e02c7802d04f04ee2f82950d3ede70f7a7be0a
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-04-18 11:09:34 +0700

    finish 142

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -22,6 +22,27 @@ navButton.addEventListener("click", function () {
   navHeader.classList.toggle("nav-open");
 });
 
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    console.log(ent);
+
+    if (!ent.isIntersecting === false) {
+      document.querySelector(".header").classList.add("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+  }
+);
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
